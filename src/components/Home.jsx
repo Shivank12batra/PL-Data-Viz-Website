3a4b3b5a8e5c3d064530e289d9c783e91e3d592f
Shivank12batra <Shivank56batra@gmail.com>
2023-03-25 20:00:07 +0530

    basic animation and styling done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,42 @@
-import {useState} from 'react';
-import { FaBars, FaTimes, FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import {useState, useEffect} from 'react';
+import { useSpring, animated } from 'react-spring';
+import { FaBars, FaTimes, FaAngleLeft, FaAngleRight, FaBackspace } from 'react-icons/fa';
+import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
+
 import home_page from '../assets/Home_Page_2.jpg';
+import home_page_3 from '../assets/home_page_3.jpg';
+import player_stats from '../assets/player_stat.jpg';
+import team_stats from '../assets/team_stats.jpg';
 import pl_logo from '../assets/pl_logo.jpg';
+import pl_logo_new from '../assets/pl_logo_new.png'
 
 const Home = () => {
-  const [navOpen, setNavOpen] = useState(false)
-
+  const bgmImages = [
+    {
+      id: 1,
+      header: 'EPL ANALYTICS',
+      content: 'Welcome to the English Premier League Data Analytics Website, a place where you get the advanced analytics coverage of the traditional top teams in the form of interactive and detailed data visualizations. Now, even fans have access to detailed and advance analytics but in an easily digestable manner!',
+      src: home_page,
+    },
+    {
+      id: 2,
+      header: 'TEAM STATS',
+      content: 'Welcome to the English Premier League Data Analytics Website, a place where you get the advanced analytics coverage of the traditional top teams in the form of interactive and detailed data visualizations. Now, even fans have access to detailed and advance analytics but in an easily digestable manner!',
+      src: team_stats,
+    },
+    {
+      id: 3,
+      header: 'PLAYER STATS',
+      content: 'Welcome to the English Premier League Data Analytics Website, a place where you get the advanced analytics coverage of the traditional top teams in the form of interactive and detailed data visualizations. Now, even fans have access to detailed and advance analytics but in an easily digestable manner!',
+      src: player_stats,
+    },
+    {
+      id: 4,
+      header: 'MATCHDAY',
+      content: 'Welcome to the English Premier League Data Analytics Website, a place where you get the advanced analytics coverage of the traditional top teams in the form of interactive and detailed data visualizations. Now, even fans have access to detailed and advance analytics but in an easily digestable manner!',
+      src: home_page_3,
+    },
+  ]
   const navItems = [
     {
       id : 1,
@@ -29,30 +60,71 @@ const Home = () => {
     },
     {
       id : 5,
-      name: 'Login',
-      href: '',
-    },
-    {
-      id : 6,
       name: 'Buy Me A Coffee',
       href: '',
     },
   ]
+
+  // Define the animations for each element using the useSpring hook
+  const titleAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 4000,
+    config: {duration: 4000} // Wait for 2 seconds before animating
+  });
+
+  const contentAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 4000,
+    config: {duration: 4000} // Wait for 4 seconds before animating
+  });
+
+  const buttonAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 6000,
+    config: {duration : 2000} // Wait for 6 seconds before animating
+  });
+  
+
+  const [navOpen, setNavOpen] = useState(false)
+  const [backgroundObj, setBackgroundObj] = useState(bgmImages[0])
+
+  useEffect(() => {
+    console.log(backgroundObj)
+    const intervalId = setInterval(() => {
+      backgroundObj.id === 4 ? setBackgroundObj(bgmImages[0]) : setBackgroundObj(bgmImages[backgroundObj.id])
+    }, 20000)
+    return () => clearInterval(intervalId)
+  }, [backgroundObj])
+
   return (
     <div className="relative h-screen">
       {/* Background Image */}
-      <div className="absolute inset-0 z-0">
+      <div className="absolute inset-0 z-11">
         <img
-          src={home_page}
+          src={backgroundObj.src}
           alt="Premier League Data Viz Website"
           className="w-full h-full object-cover"
         />
       </div>
-
+      {/* title */}
+      {/* Render animated content */}
+      <animated.h1 style={titleAnimation} className='absolute top-16 left-1/2 transform -translate-x-1/2'><span className='text-white text-3xl font-bold inline-block bg-black px-20 py-4 text-center sm:text-5xl z-10'>{backgroundObj.header}</span></animated.h1>
+        <animated.div className='absolute top-2/3 left-1/3 transform -translate-x-1/2 -translate-y-1/2 z-0 ml-8'>
+          <button className='group text-white font-bold text-sm bg-black px-4 py-2 my-2 hover:bg-red-500 hover:shadow-lg hover:-translate-x-1 transition-all duration-200'>
+            BROWSE SECTION
+            <span className='hidden align-middle ml-2 group-hover:inline-block'>
+              <MdOutlineKeyboardArrowRight size={25}/>
+            </span>
+          </button>
+          <animated.p className='text-white text-sm bg-black max-w-md px-2 py-2 sm:text-lg'>{backgroundObj.content}</animated.p>
+        </animated.div>
       {/* Logo */}
       <div className="absolute top-2 left-0 z-10 sm:left-24 sm:top-12 cursor-pointer">
         <img
-          src={pl_logo}
+          src={pl_logo_new}
           alt="Premier League Logo"
           className="w-20 text-6xl"
         />
@@ -66,14 +138,18 @@ const Home = () => {
         </button>
       </div>
       {/* image slide button */}
-      <div className="absolute right-6 top-1/2 transform -translate-y-1/2">
-        <button className="bg-gray-400 rounded-full p-2 hover:bg-gray-300 transition-colors duration-300">
-          <FaAngleRight size={24} />
+      <div className="absolute right-0 top-1/2 transform -translate-y-1/2 sm:right-6">
+        <button className="bg-gray-500 p-2 hover:bg-gray-300 transition-colors duration-300">
+          <FaAngleRight size={24} onClick={() => {
+            backgroundObj.id === 4 ? setBackgroundObj(bgmImages[0]) : setBackgroundObj(bgmImages[backgroundObj.id])
+          }}/>
         </button>
       </div>
-      <div className="absolute left-6 top-1/2 transform -translate-y-1/2">
-        <button className="bg-gray-400 rounded-full p-2 hover:bg-gray-300 transition-colors duration-300">
-          <FaAngleLeft size={24} />
+      <div className="absolute left-0 top-1/2 transform -translate-y-1/2 sm:left-6">
+        <button className="bg-gray-400 p-2 hover:bg-gray-300 transition-colors duration-300">
+          <FaAngleLeft size={24}  onClick={() => {
+            backgroundObj.id === 1 ? setBackgroundObj(bgmImages[bgmImages.length - 1]) : setBackgroundObj(bgmImages[backgroundObj.id - 2])
+          }}/>
         </button>
       </div>
       {navOpen && (
@@ -88,3 +164,4 @@ const Home = () => {
 };
 
 export default Home
+
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,15 @@ module.exports = {
     "./src/**/*"
   ],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      // You can add more breakpoints here
+    },
+    extend: {
+      textSizes: {
+        'md': '1.125rem', // You can adjust this to your desired size
+      }
+    },
   },
   plugins: [],
 }
