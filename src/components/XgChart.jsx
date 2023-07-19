cd950163cc6ed96b28b33e8c05392f6b04798fd3
Shivank12batra <Shivank56batra@gmail.com>
2023-07-19 22:13:02 +0530

    tooltip + axis labels added

--- src/components/XgChart.jsx
+++ src/components/XgChart.jsx
@@ -8,14 +8,7 @@ const XgChart = () => {
   const chartRef = useRef(null)
 
   const {data, isLoading, error, refetch} = fetchCumulativeXGChartData('Arsenal', 'Arsenal', 'Tottenham')
-
-  const handleMouseOver = () => {
-
-  }
-
-  const handleMouseOut = () => {
-
-  }
+  
 
   const drawChart = () => {
     const chartContainer = d3.select(chartRef.current);
@@ -23,10 +16,34 @@ const XgChart = () => {
     // Clear previous chart
     chartContainer.selectAll('*').remove();
 
+    const handleMouseOver = (event, d) => {
+        const shotxG = parseFloat(d.xG)
+        const roundedxG = shotxG.toFixed(2)
+        console.log(d)
+        const tooltip = d3.select('body')
+          .append('div')
+          .attr('id', 'tooltip')
+          .attr('class', 'absolute bg-black bg-opacity-70 text-white text-xs rounded p-2 z-10')
+          .html(`
+            <p>Player Name: ${d.player}</p>
+            <p>xG: ${roundedxG}</p>
+            <p>Minute: ${d.minute}</p>
+            <p>Result: ${d.result}</p>
+            <p>Situation: ${d.situation}</p>
+          `);
+          tooltip.style("top", event.pageY + "px")
+        .style("left", event.pageX + "px")
+      };
+      
+      const handleMouseOut = () => {
+        console.log('mouse out')
+        d3.selectAll('#tooltip').remove();
+      };
+
     // Define chart dimensions
     const width = 400;
     const height = 400;
-    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const margin = { top: 20, right: 20, bottom: 40, left: 50 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
 
@@ -50,12 +67,13 @@ const XgChart = () => {
     const xScale = d3.scaleLinear().domain([d3.min(homeMins), d3.max(homeMins) + 3]).range([0, innerWidth]);
     const yScale = d3.scaleLinear().domain([0, d3.max(combinedCumulativeXGValues)]).range([innerHeight, 0]);
 
-    // Create line generator
+    // home cumulative XG line
     const homeLine = d3.line()
       .x((_, i) => xScale(homeMins[i]))
       .y((d) => yScale(d))
       .curve(d3.curveStepAfter)
-
+    
+    // away cumulative XG line
     const awayLine = d3.line()
       .x((_, i) => xScale(awayMins[i]))
       .y((d) => yScale(d))
@@ -97,7 +115,7 @@ const XgChart = () => {
         .attr('transform', (d) => `translate(${xScale(+d.minute) - 10},${yScale(d.cumulativeXG) - 10})`)
         .on('mouseover', handleMouseOver)
         .on('mouseout', handleMouseOut)
-        .attr('fill', 'gold');
+        .attr('fill', 'gold')
 
     // away goals plotted
     svg.selectAll('.away-shot-dot')
@@ -108,8 +126,28 @@ const XgChart = () => {
         .attr('d', starSymbol)
         .attr('transform', (d) => `translate(${xScale(+d.minute) - 10},${yScale(d.cumulativeXG) - 10})`)
         .on('mouseover', handleMouseOver)
-        .on('mouseout', handleMouseOut)
+        .on('mouseleave', handleMouseOut)
         .attr('fill', 'gold');
+
+    // Add x-axis label
+    svg.append('text')
+    .attr('class', 'x-axis-label')
+    .attr('x', innerWidth / 2)
+    .attr('y', innerHeight + margin.top + 20)
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'white')
+    .text('Minutes');
+
+    // Add y-axis label
+    svg.append('text')
+    .attr('class', 'y-axis-label')
+    .attr('transform', 'rotate(-90)')
+    .attr('x', -innerHeight / 2)
+    .attr('y', -margin.left)
+    .attr('dy', '1em')
+    .attr('text-anchor', 'middle')
+    .attr('fill', 'white')
+    .text('Cumulative xG');
 
     // Create x-axis
     const xAxis = d3.axisBottom(xScale);
@@ -118,16 +156,17 @@ const XgChart = () => {
       .attr('class', 'x-axis')
       .attr('transform', `translate(0, ${innerHeight})`)
       .call(xAxis)
-      .attr('class', 'stroke-current text-white');
+      .attr('class', 'text-white');
 
     // Create y-axis
     const yAxis = d3.axisLeft(yScale);
+
     svg
       .append('g')
       .attr('class', 'y-axis')
       .call(yAxis)
-      .attr('class', 'stroke-current text-white');
-  };
+      .attr('class', 'text-white');
+    };
 
   useEffect(() => {
     if (data) {
@@ -143,7 +182,7 @@ const XgChart = () => {
     return <div>Something went wrong!</div>
   }
 
-  return <div ref={chartRef} />
+  return <div ref={chartRef} className='ml-16 mt-12 h-screen'/>
 }
 
 export default XgChart
