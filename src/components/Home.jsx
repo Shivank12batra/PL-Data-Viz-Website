1e86aa4e6e3f47d76d827b3305ed6837b9b1d093
Shivank12batra <Shivank56batra@gmail.com>
2023-07-28 17:37:28 +0530

    refactored code + teams data added to select tag

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,47 +3,9 @@ import { useSpring, useTransition, animated } from 'react-spring';
 import { FaAngleLeft, FaAngleRight} from 'react-icons/fa';
 import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
 import NavBar from './NavBar';
-
-import home_page from '../assets/home_page.png';
-import team_stats from '../assets/team_stats.png';
-import player_stats from '../assets/player_stats.png';
-import matchday from '../assets/matchday.png';
-import glossary from '../assets/glossary.png';
+import bgmImages from '../data/animationContent';
 
 const Home = ({navOpen, setNavOpen}) => {
-  const bgmImages = [
-    {
-      id: 1,
-      header: 'EPL ANALYTICS',
-      content: 'Your ultimate source for in-depth analysis of the English Premier League. Get access to comprehensive statistics, player and team performance metrics, and expert insights to stay ahead of the game. Perfect for passionate fans who want to deepen their knowledge and understanding of the sport!',
-      src: home_page,
-    },
-    {
-      id: 2,
-      header: 'TEAM STATS',
-      content: "Get the inside scoop on your favorite teams with our Team Stats section. Our interactive visualizations like pass networks, shot maps, and more, give you comprehensive insights into team performance. Get ready to geek out on data and take your fandom to the next level!",
-      src: team_stats,
-    },
-    {
-      id: 3,
-      header: 'PLAYER STATS',
-      content: "Have you ever wondered what makes your favorite players stand out on the field? Our Player Stats section is your one-stop-shop for in-depth insights and comprehensive statistics on each player's performance. From pinpoint passing to clutch goals, find out what makes your favourite player special!",
-      src: player_stats,
-    },
-    {
-      id: 4,
-      header: 'MATCHDAY',
-      content: 'Get exclusive access to the latest match insights with our Matchday section. From the standout performers to the crucial moments of the game, our detailed visualizations bring you closer to the action than ever before. Experience the thrill of matchday like never before',
-      src: matchday,
-    },
-    {
-      id: 5,
-      header: 'GLOSSARY',
-      content: "This section serves as a comprehensive guide to help you understand the key analytic terms used in our website. Whether you're a seasoned fan or new to the game, our glossary is the perfect reference tool to gain a deeper understanding of the analytics behind the sport.",
-      src: glossary,
-    },
-  ]
-
   const [headerVisible, setHeaderVisible] = useState(false)
   const [contentVisible, setContentVisible] = useState(false)
   const [buttonVisible, setButtonVisible] = useState(false)
--- src/components/TeamStats.jsx
+++ src/components/TeamStats.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TeamTable from './TeamTable'
 import XgChart from '../dataviz/XgChart'
 import PassingNetwork from '../dataviz/PassingNetwork'
 import TeamShotMap from '../dataviz/TeamShotMap'
 import { QueryTest } from './QueryTest'
+import { useAuth } from '../context/AuthContext'
+import data from '../data/table'
 
 const TeamStats = () => {
+  const {team} = useAuth()
+  const [oppositionTeam, setOppositionTeam] = useState(team === data[0].Squad ? data[1].Squad : data[0].Squad)
+  const [selectedData, setSelectedData] = useState({
+    oppositionTeam: team === data[0].Squad ? data[1].Squad : data[0].Squad,
+    venue: 'Home'
+  })
+
+  const handleInputChange = (event) => {
+    const {name, value} = event.target
+    console.log(value)
+    setSelectedData((previousData) => ({
+      ...previousData,
+      [name]: value,
+    }))
+  }
+
   return (
     <div className='bg-gradient-to-b from-black to-gray-800 border-2 border-solid border-transparent'>
       <TeamTable/>
@@ -15,15 +33,24 @@ const TeamStats = () => {
         <div className='flex flex-col justify-center items-center items-between m-4 sm:flex-row sm:justify-between'>
           <div>
             <h3 className='text-white text-xl text-center font-bold mb-2'>Against</h3>
-            <select className='px-12 py-2 border border-gray-400 rounded-lg shadow-md hover:cursor-pointer'>
-              <option>22-23</option>
+            <select className='px-4 py-2 text-center border border-gray-400 rounded-lg shadow-md hover:cursor-pointer'
+            name='oppositionTeam'
+            value={selectedData.oppositionTeam}
+            onChange={handleInputChange}>
+            {data.map(teamObj => { 
+              const { Rank, Squad } = teamObj
+              return Squad !== team ? <option key={Rank} className='bg-gray-800 hover:cursor-pointer text-white'>{Squad}</option> : null
+            })}
             </select>
           </div>
           <div className='sm:ml-4 ml-0 mt-4 sm:mt-0'>
             <h3 className='text-white text-xl text-center font-bold mb-2'>Venue</h3>
-            <select className='px-12 py-2 border border-gray-400 rounded-lg shadow-md hover:cursor-pointer'>
-              <option>Home</option>
-              <option>Away</option>
+            <select className='px-12 py-2 border border-gray-400 rounded-lg shadow-md hover:cursor-pointer'
+            name='venue'
+            value={selectedData.venue}
+            onChange={handleInputChange}>
+              <option className='bg-gray-800 hover:cursor-pointer text-white'>Home</option>
+              <option className='bg-gray-800 hover:cursor-pointer text-white'>Away</option>
             </select>
           </div>
         </div>
@@ -38,5 +65,3 @@ const TeamStats = () => {
 }
 
 export default TeamStats
-
-// bg-gradient-to-b from-black to-gray-800
