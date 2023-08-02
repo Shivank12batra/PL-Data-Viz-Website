d08f5f7b2ac6b0443bcf002e76006702acafbb23
Shivank12batra <Shivank56batra@gmail.com>
2023-08-02 15:35:00 +0530

    fix: re-rendering of passing network chart fixed for a smooth ux

--- src/hooks/getPassingNetworkData.js
+++ src/hooks/getPassingNetworkData.js
@@ -2,5 +2,8 @@ import { useQuery } from "react-query";
 import { teamPassingNetworkData } from "../firestore/getTeamStats";
 
 export const fetchPassingNetworkData = (...args) => {
-    return useQuery(['passingNetworkData', ...args], () => teamPassingNetworkData(args))
+    return useQuery(['passingNetworkData', ...args], () => teamPassingNetworkData(args), {
+        refetchOnWindowFocus: false,
+        keepPreviousData: true,
+    })
 }
