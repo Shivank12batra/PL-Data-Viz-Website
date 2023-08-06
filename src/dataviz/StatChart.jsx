97cdcef642f9ad3e67fdcece77a81f5b2cf4535a
Shivank12batra <Shivank56batra@gmail.com>
2023-08-06 15:28:45 +0530

    fix: responsive issue fixed player bar chart

--- src/dataviz/StatChart.jsx
+++ src/dataviz/StatChart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import Loader from '../components/Loader';
 import Error from '../components/Error';
@@ -10,12 +10,18 @@ const StatChart = () => {
   const chartRef = useRef()
   const { team } = useAuth()
   const { stat } = useParams()
-  const height = 300
+  const [height, setHeight] = useState(getHeight())
   const width = 600
+
+  function getHeight() {
+    return window.innerWidth > 540 ? 300 : 600
+  }
   
   const transformStat = stat === 'Pass%20Completion%20%' ? 'Pass Completion %' : stat
 
   const { data, isLoading, error } = fetchTopPlayersData(team, transformStat)
+
+  console.log(window.innerWidth)
 
   const barChart = () => {
     const chartContainer = d3.select(chartRef.current)
@@ -34,7 +40,7 @@ const StatChart = () => {
 
     const svg = chartContainer
                 .append('svg')
-                .attr('viewBox', `0 0 ${width} ${height}`) 
+                .attr('viewBox', `15 15 ${width} ${height}`)
                 .attr('preserveAspectRatio', 'xMidYMid meet') 
                 .append('g')
                 .attr('transform', `translate(${margin.left}, ${margin.top})`)
@@ -49,7 +55,7 @@ const StatChart = () => {
     const yScale = d3.scaleLinear().domain([0, d3.max(chartData, (d) => d.value + 5)]).range([innerHeight, 0])
 
     // X-Axis
-    const xAxis = d3.axisBottom(xScale).tickSizeOuter(0).tickPadding(4)
+    const xAxis = d3.axisBottom(xScale).tickSizeOuter(0).tickPadding(5)
 
     svg
       .append('g')
@@ -95,6 +101,18 @@ const StatChart = () => {
   }
 
   useEffect(() => {
+    const updateHeight = () => {
+        setHeight(getHeight())
+    }
+    window.addEventListener('resize', updateHeight);
+
+
+    return(() => {
+        window.removeEventListener('resize', updateHeight);
+    })
+}, [height])
+
+  useEffect(() => {
     if (data) {
       barChart()
     }
@@ -114,8 +132,8 @@ const StatChart = () => {
 
   return (
     <div className='bg-gradient-to-b from-black to-gray-800 border-2 border-solid border-transparent min-h-screen'>
-      <h1 className='text-white text-4xl font-bold mb-2 mx-auto underline underline-offset-4 text-center mt-32'>{transformStat.toUpperCase()} CHART</h1>
-      <div ref={chartRef} className='flex justify-center mb-8 w-full h-300 border-2'/>
+      <h1 className='text-white text-2xl sm:text-4xl font-bold mb-2 mx-auto underline underline-offset-4 text-center mt-32'>{transformStat.toUpperCase()} CHART</h1>
+      <div ref={chartRef} className='flex justify-center mb-8'/>
     </div>
   )
 }
