0925c75f9839cea0b392aa5b528248d68b1ec9c8
Shivank12batra <Shivank56batra@gmail.com>
2023-08-14 02:30:25 +0530

    feat: glossary graphs explainer section

--- src/components/Glossary.jsx
+++ src/components/Glossary.jsx
@@ -9,7 +9,7 @@ const Glossary = () => {
       <h1 className='text-white text-4xl font-bold mt-32 mb-6 mx-auto underline underline-offset-4 text-center'>
         GLOSSARY
       </h1>
-      <div className='text-white w-4/5 mx-auto my-8'>
+      <div className='text-white text-center sm:text-left sm:w-4/5 mx-auto my-8'>
         <p>
           Welcome to our Football Analytics Glossary! If you're a casual football fan, you might have been perplexed by terms like xG, psxG, passing network, and more. Don't worry; we've got you covered! These seemingly intimidating terms often reveal meaningful insights that can significantly enhance your understanding of the fast-paced world of football.
         </p>
@@ -18,7 +18,7 @@ const Glossary = () => {
         </p>
         <div className='my-8'>
           <h1 className='text-2xl text-center font-bold mb-4 underline underline-offset-4'>
-            Key Terms
+            KEY TERMS
           </h1>
           <p className='font-bold underline underline-offset-4 mb-2'>xG (Expected Goals):</p>
           <p className='my-4'>
@@ -57,6 +57,66 @@ const Glossary = () => {
               https://saturdays.substack.com/p/game-states-and-when-expected-goals-arent-enough
             </a>
           </div>
+          <p className='my-4'>
+            Now, if you have already understood about the concept of xG then it would become really easy to comprehend about metrics/charts that are built on top of that concept.
+          </p>
+          <p className='font-bold underline underline-offset-4 my-2'>PSxG (Post Shot Expected Goals):</p>
+          <p className='my-4'>
+            The next metric is post shot xG(PSxG). Post-Shot Expected Goals (PsxG) is a statistic that gives us an idea of how likely a shot will result in a goal, even after the shot has been taken. This takes into account various factors like where the shot was taken from, the type of shot, and the abilities of the goalkeeper. It's like a way to measure the quality of a scoring opportunity.
+          </p>
+          <p className='my-4'>
+            Example, imagine a striker takes a shot from a difficult angle, but the goalkeeper makes a diving save and prevents the ball from going into the net. Even though the shot looked challenging, the goalkeeper's save means that the shot didn't turn into a goal. PsxG helps us understand whether that shot was actually difficult to save or if the goalkeeper's skill made the difference.
+          </p>
+          <p className='my-4'>
+            Use Case of PsxG - Goalkeeper Analysis (GA):
+            PsxG can be used to analyze goalkeepers in a more detailed way. Goalkeeper Analysis (GA) using PsxG involves comparing a goalkeeper's actual saves with the expected goals from the shots they faced. If a goalkeeper consistently makes saves that are higher than the expected goals of those shots, it suggests they are performing exceptionally well. On the other hand, if a goalkeeper concedes goals from shots with low expected goals, it might indicate areas for improvement in their positioning or decision-making.
+          </p>
+          <h1 className='text-2xl text-center font-bold mb-4 underline underline-offset-4'>
+            HOW TO INTEPRET THE GRAPHS?
+          </h1>
+          <p className='my-4'>
+            Again, if you have already understood the concept of xG, charts such as cumulative xG and shotmap would start to make sense. But there are other charts that might still need some amount of explaination and for that, we have got you covered as well!
+          </p>
+          <p className='font-bold underline underline-offset-4 my-2'>
+            Passing Network Chart:
+          </p>
+          <p className='my-4'>
+            The passing network chart showcases the average positions of the players (when they received passes) on the pitch and the passing relationship/dynamics between the team players. The stronger the dynamic the thicker is the line connecting them.
+          </p>
+          <img src={networkChart} alt='xGChart' className='my-4 mx-auto' width={300} height={300}/>
+          <p className='font-bold underline underline-offset-4 my-2'>
+            Player Report Stats:
+          </p>
+          <p className='my-4'>
+            The player report on the player stats page has a toggle to switch between percentile or per 90 minutes. 
+          </p>
+          <p className='my-4'>
+            Percentile section is the percentile rank shown for that particular player in that particular stat compared to players of the same position in the whole league. Let's say Harry Kane, a prominent striker for Tottenham Hotspur, has an xG percentile value of 95 in the Premier League for a specific season. This means that out of all the strikers (players in the same position) who have participated in the Premier League during that season, Harry Kane's xG value is higher than approximately 85% of them. In simpler terms, he ranks better than 95% of his fellow strikers in terms of xG.
+          </p>
+          <p className='my-4'>
+            On the other hand, the "per 90 minutes" statistic is a way of standardizing and comparing player performance. This is often used to provide a fair comparison of players' contributions, regardless of the amount of playing time they have had. Imagine you have two players, Player A and Player B. Player A has played in 10 matches, each lasting a different amount of time, while Player B has played in 5 matches. If you just compare their total goals or assists, it might not accurately reflect their actual contributions because Player A might have played more minutes overall.
+          </p>
+          <p className='my-4'>
+            Here's how you'd calculate a statistic per 90 minutes:
+          </p>
+          <p>
+            For Player A: Total goals = 8, Total minutes played = 600 minutes
+          </p>
+          <p>
+            For Player B: Total goals = 4, Total minutes played = 270 minutes
+          </p>
+          <p className='my-4'>
+            Calculate Goals Per 90 Minutes:
+          </p>
+          <p>
+            For Player A: (8 goals / 600 minutes) * 90 = 1.2 goals per 90 minutes
+          </p>
+          <p>
+            For Player B: (4 goals / 270 minutes) * 90 = 1.33 goals per 90 minutes
+          </p>
+          <p className='my-4'>
+            I hope that we were able to explain all the key terms and that you would have a smooth experience navigating the website. If in case, there is anything that you need more clarity on, you can contact me <a href='https://www.linkedin.com/in/shivank-batra-4594b9202/' target='_blank' className='underline font-bold'>here</a>
+          </p>
         </div>
       </div>
     </div> 
