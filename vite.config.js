59f38fef590b8178c587864e8e518bf04e1c43e9
Shivank12batra <Shivank56batra@gmail.com>
2023-08-15 00:25:45 +0530

    chore: vite.config.js configured

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  outDir: 'dist', // Output directory for the build
+  assetsDir: 'assets', // Directory for static assets like images, etc.
+  minify: true,
 });
 
